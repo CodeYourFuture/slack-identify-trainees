e37cfab75de0d96247f85d0ae91bf0a4ef5dc0be
Haroon <haroon.ali.1104@gmail.com>
2023-11-28 15:58:39 +0000

    send message to user if profile field not set

--- netlify/functions/tutorial-app.js
+++ netlify/functions/tutorial-app.js
@@ -11,21 +11,28 @@ const app = new App({
 });
 
 app.message(async ({ message, client }) => {
-    try {
-        // Open a direct message channel with the user
-        const result = await client.conversations.open({
-            users: message.user,
-        });
+    // Check if the message is from a user in a channel (not a bot message)
+    if (message.subtype !== 'bot_message' && message.user) {
+        try {
+            // Retrieve user profile information
+            const userProfile = await client.users.profile.get({
+                user: message.user,
+            });
 
-        // Send a hello message
-        await client.chat.postMessage({
-            channel: result.channel.id,
-            text: 'Hello!',
-        });
-    } catch (error) {
-        console.error(error);
+            // Check if the 'Is Trainee' field is empty
+            if (!userProfile.profile.fields['Xf065B6QU58A']) {
+                // Send a message to the user about the missing profile field
+                await client.chat.postMessage({
+                    channel: message.user,
+                    text: 'Please make sure to set your "Is Trainee" profile field.',
+                });
+            }
+        } catch (error) {
+            console.error('Error checking user profile:', error);
+        }
     }
 });
+
 
 module.exports.handler = async (event, context, callback) => {
     const handler = await awsLambdaReceiver.start();
