1c9a4a8ec01c59b035285efe4eefbd90577f01ba
Haroon <haroon.ali.1104@gmail.com>
2023-12-04 13:53:40 +0000

    create first for user joining workspace

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,35 +1,22 @@
 import crypto from 'crypto';
 import { server, sentRequest, handlers } from './server.js';
-import pkg from '@slack/bolt'; const { App, AwsLambdaReceiver } = pkg;
 import { boltApp, initApp, handler } from '../netlify/functions/slack-app.js';
+import pkg from '@slack/bolt'; const { App, AwsLambdaReceiver } = pkg;
 import teamJoinRequest from '../requests/team_join.json' assert { type: 'json' };
 
 boltApp.awsLambdaReceiver = new AwsLambdaReceiver({
-    signingSecret: 'my-secret',
+    signingSecret: 'slackSecret',
 });
 
 boltApp.app = new App({
-    token: 'SLACK_TOKEN',
+    token: 'slackToken',
     receiver: boltApp.awsLambdaReceiver
 });
 
 initApp(boltApp);
 
-// async function sendRequest() {
-//     server.use(...handlers.teamJoin);
-
-//     const result = await handler(createSlackRequest(teamJoinRequest));
-//     console.log(result);
-//     console.log(sentRequest.payload);
-// }
-
-// sendRequest();
-
-
 describe('slack app', () => {
-    it('returns 200 after receiving message event', async () => {
-        // server.use(...handlers.teamJoin);
-
+    it('sends a message when a user joins the workspace', async () => {
         const result = await handler(createSlackRequest(teamJoinRequest));
         expect(result.statusCode).toBe(200);
         expect(sentRequest.payload.text).toBe('Welcome! Please make sure to set your "Is Trainee" profile field.');
@@ -40,7 +27,7 @@ function createSlackRequest(data, path = '/slack/events') {
     const body = JSON.stringify(data);
     const timestamp = Math.floor(Date.now() / 1000);
     const signature = crypto
-        .createHmac('sha256', 'my-secret')
+        .createHmac('sha256', 'slackSecret')
         .update(`v0:${timestamp}:${body}`)
         .digest('hex');
 
--- tests/server.js
+++ tests/server.js
@@ -17,13 +17,7 @@ export const handlers = {
 
 export const server = setupServer(...handlers.teamJoin);
 
-beforeAll(() => {
-    server.listen({
-        onUnhandledRequest: ({ method, url }) => {
-            throw new Error(`Unhandled ${method} request to ${url}`);
-        },
-    });
-});
+server.listen();
 
 beforeEach(() => {
     server.resetHandlers();
